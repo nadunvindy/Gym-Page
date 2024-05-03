34433203f9917f2db1676647c5321ad47adc0809
Nadun <nadunbalasuriya03@gmail.com>
2024-05-03 14:10:00 +0800

    Add styling to home page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "./styles.css"
+
 let content = document.querySelector("#content");
 let title = document.createElement("h1");
 let subtitle = document.createElement("h3");
@@ -10,7 +12,11 @@ let locCard1 = document.createElement("div");
 let locCard2 = document.createElement("div");
 let locations = document.createElement("div");
 
+locCard1.className = "locCard1"
+locCard2.className = "locCard2"
+locations.className = "locations"
 title.textContent = "Welcome to MMA Gym";
+title.className = "title"
 subtitle.textContent =
   "Where Warriors Are Forged: Unleash Your Inner Strength at MMA Gym!";
 locationsTitle.textContent = "Locations";
